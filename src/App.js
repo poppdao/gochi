e9e2bf06b41f47704bc6a2b5c94bfb59f97a4e48
Fred Lin <gasolin@gmail.com>
2022-01-14 17:20:34 +0800

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="text-5xl font-bold text-green-500">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
